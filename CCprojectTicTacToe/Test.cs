f126b6cbba4ef7d0e61ed11cb014e19037175c4c
EmperorProbus <kkowalczyk730@gmail.com>
2024-01-23 18:27:33 +0100

    Add unit test

---
+++ CCprojectTicTacToe/Test.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CCproject;
+
+public class Test {
+	[Fact]
+	public void DatabaseConnectTest() {
+		DatabaseConnect db = new DatabaseConnect();
+
+		Assert.True(db.spinning == false);
+		Assert.False(db.game_info is not null);
+	}
+}
